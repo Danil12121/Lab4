86c3b2c6abe7e812426d84f6a969a4964b5b7ae4
Danil12121 <95086085+Danil12121@users.noreply.github.com>
2024-10-20 20:09:17 +0300

    Create task6.cpp

---
+++ task6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string replace(const string str, const string old, const string new_str){
+    string s = str;
+    while (s.find(old) != string::npos){
+        int f = s.find(old);
+        s = s.substr(0, f) + new_str + s.substr(f + old.length(),s.length());
+    }
+    return s;
+}
+
+int main()
+{
+    string newString = replace("Can you can a can as a canner can can a can?", "can", "cAN");
+    cout << newString;
+    return 0;
+}
